f52572cfd7268140eb6c9a1362a4389502d6a506
ncanhbao <ncanhbao@gmail.com>
2023-12-10 07:18:47 +0700

    Xu ly path param va query params

--- app/index.py
+++ app/index.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, request
 from app import app, module
 
 
@@ -9,9 +9,18 @@ def home():
 
 @app.route("/flight")
 def flight_list():
-    flight = module.load_flight()
+    cate_id = request.args.get("flight_type")
+    fr = request.args.get("from")
+    to = request.args.get("to")
+    flight = module.load_flight(cate_id=cate_id, fr=fr, to=to)
 
-    return render_template('flight.html', products=flight)
+    return render_template('flight.html', flights=flight)
+
+
+@app.route("/flight/<int:flight_id>")
+def flight_detail(flight_id):
+    flight = module.get_flight_by_id(flight_id)
+    return render_template('flight_detail.html', flight=flight)
 
 
 if __name__ == '__main__':
--- app/module.py
+++ app/module.py
@@ -6,7 +6,24 @@ def read_json(path):
         return json.load(f)
 
 def load_flighttype():
-    return read_json(os.path.join(app.root_path, 'data/flighttype.jason'))
+    return read_json(os.path.join(app.root_path, 'data/flighttype.json'))
 
-def load_flight():
-    return read_json(os.path.join(app.root_path, 'data/flight.jason'))
+def load_flight(cate_id=None, fr=None, to=None):
+    flights = read_json(os.path.join(app.root_path, 'data/flight.json'))
+
+    if cate_id:
+        flights = [f for f in flights if f['flight_type'] == int(cate_id)]
+
+    if fr and to:
+        flights = [f for f in flights if f['from'].lower().find(fr.lower()) >= 0
+                   and f['to'].lower().find(to.lower()) >= 0]
+
+    return flights
+
+def get_flight_by_id(flight_id=None):
+    flights = read_json(os.path.join(app.root_path, 'data/flight.json'))
+    for f in flights:
+        if f['id'] == flight_id:
+            return f
+
+    return None
